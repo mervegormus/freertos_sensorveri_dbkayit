607e404c5c9d552fb571a6af0a0e0abaaedbc387
mervegormus <mervegormus948@gmail.com>
2021-02-21 20:50:57 +0300

    realtimesys_datatranfer

---
+++ realtimesys_datatransfer/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace realtimesys_datatransfer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.durdur = new System.Windows.Forms.Button();
+            this.sifirla = new System.Windows.Forms.Button();
+            this.kaydet = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.baslat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // durdur
+            // 
+            this.durdur.Location = new System.Drawing.Point(404, 30);
+            this.durdur.Name = "durdur";
+            this.durdur.Size = new System.Drawing.Size(75, 23);
+            this.durdur.TabIndex = 1;
+            this.durdur.Text = "Durdur";
+            this.durdur.UseVisualStyleBackColor = true;
+            this.durdur.Click += new System.EventHandler(this.durdur_Click);
+            // 
+            // sifirla
+            // 
+            this.sifirla.Location = new System.Drawing.Point(509, 30);
+            this.sifirla.Name = "sifirla";
+            this.sifirla.Size = new System.Drawing.Size(75, 23);
+            this.sifirla.TabIndex = 2;
+            this.sifirla.Text = "Sıfırla";
+            this.sifirla.UseVisualStyleBackColor = true;
+            this.sifirla.Click += new System.EventHandler(this.sifirla_Click);
+            // 
+            // kaydet
+            // 
+            this.kaydet.Location = new System.Drawing.Point(609, 28);
+            this.kaydet.Name = "kaydet";
+            this.kaydet.Size = new System.Drawing.Size(75, 25);
+            this.kaydet.TabIndex = 5;
+            this.kaydet.Text = "Kaydet";
+            this.kaydet.UseVisualStyleBackColor = true;
+            this.kaydet.Click += new System.EventHandler(this.kaydet_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(40, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(213, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // baslat
+            // 
+            this.baslat.Location = new System.Drawing.Point(290, 30);
+            this.baslat.Name = "baslat";
+            this.baslat.Size = new System.Drawing.Size(75, 23);
+            this.baslat.TabIndex = 8;
+            this.baslat.Text = "Başlat";
+            this.baslat.UseVisualStyleBackColor = true;
+            this.baslat.Click += new System.EventHandler(this.baslat_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Port:";
+            // 
+            // listView1
+            // 
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(404, 59);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(355, 360);
+            this.listView1.TabIndex = 12;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 59);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(369, 360);
+            this.textBox1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.baslat);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.kaydet);
+            this.Controls.Add(this.sifirla);
+            this.Controls.Add(this.durdur);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button durdur;
+        private System.Windows.Forms.Button sifirla;
+        private System.Windows.Forms.Button kaydet;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button baslat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ realtimesys_datatransfer/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace realtimesys_datatransfer
+{
+    public partial class Form1 : Form
+    {
+        private string data;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            listView1.View = View.List;
+            listView1.FullRowSelect = true;
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+                comboBox1.Items.Add(port);
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(SerialPort1_DataReceived);
+
+
+        }
+
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            data = serialPort1.ReadLine();
+            this.Invoke(new EventHandler(displayData_event));
+        }
+
+        private void displayData_event(object sender, EventArgs e)
+
+        {
+            textBox1.Text += DateTime.Now.ToShortDateString()+ "\t\t" + DateTime.Now.ToLongTimeString()+"\t\t"+ data + "\n"; 
+            listView1.Items.Add(data);
+        }
+
+        private void baslat_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.BaudRate = 9600;
+                serialPort1.Open();
+                durdur.Enabled = true;
+                baslat.Enabled = false;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "HATA");
+            }
+        }
+
+        private void durdur_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            durdur.Enabled = false;
+            baslat.Enabled = true;
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+                serialPort1.Close();
+        }
+        private void sifirla_Click(object sender, EventArgs e)
+        {
+           
+            listView1.Clear();
+            textBox1.Clear();
+           
+        }
+        private void kaydet_Click(object sender, EventArgs e)
+        {
+            //txt dosyasına kaydetme işlemi
+             try
+             {
+                 for(int i=0; i < listView1.Items.Count; i++)
+                {
+                    string path = @"D:\\";
+                    string filename = "data.txt";
+                    System.IO.File.WriteAllText(path + filename, "Tarih\t\tZaman\t\tSensor\t\tDeger\n"+textBox1.Text);
+                }
+                 
+                 MessageBox.Show("Dosya txt olarak kaydedildi", "Mesaj");
+             }
+             catch (Exception ex2)
+             {
+                 MessageBox.Show(ex2.Message, "txt dosyası");
+             }   
+          //db kayıt
+            int etkilenen = 0;
+
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                try
+                {
+                    SqlConnection conn = new SqlConnection
+                    ("Data Source=uemyodata.database.windows.net;Initial Catalog=realtimeSys;User ID=ueadmin;Password=Merve1234");
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = conn;
+                    string sensor_adı = listView1.Items[i].SubItems[0].Text;
+                    string sensor_deger = listView1.Items[i].SubItems[0].Text;
+                    string tarih = DateTime.Now.ToShortDateString();
+                    string saat = DateTime.Now.ToLongTimeString();
+                    cmd.CommandType = System.Data.CommandType.Text;
+
+                    cmd.CommandText = "insert into sensor_bilgi (tarih,saat,sensor_adı,sensor_deger) values (@tarih, @saat,@sensor_adı,@sensor_deger)";
+
+                    cmd.Parameters.AddWithValue("@tarih", tarih);
+                    cmd.Parameters.AddWithValue("@saat", saat);
+                    cmd.Parameters.AddWithValue("@sensor_adı", sensor_adı);
+                    cmd.Parameters.AddWithValue("@sensor_deger", sensor_deger);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    
+                    conn.Close();
+
+                }
+                catch (Exception hata)
+                {
+                    MessageBox.Show( hata.Message);
+                }
+            }
+            if (etkilenen != 0) 
+            {
+                MessageBox.Show("DB kayıt edilmedi");
+            }
+            else
+            {
+                MessageBox.Show("DB kayıt edildi");
+
+            }
+        }
+
+        
+    }
+}
